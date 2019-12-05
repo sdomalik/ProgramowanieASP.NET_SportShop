32bd644d8760a601ab21162e07c045b193d751f0
Szymon Domalik <szymon.domalik@gmail.com>
2019-12-05 23:00:37 +0100

    Added Login View and Login method in Account Controller

--- SportShop/Controllers/AccountController.cs
+++ SportShop/Controllers/AccountController.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using SportShop.Models.ViewModels;
 
 namespace SportShop.Controllers
 {
+    [Authorize]
+
     public class AccountController : Controller
     {
         private readonly UserManager<IdentityUser> _userManager;
@@ -21,15 +25,49 @@ namespace SportShop.Controllers
             return View();
         }
 
-        /*public async Task<IActionResult> Login(string email, string password)
-        {
-            if (_userManager.FindByNameAsync(email) != null)
-            {
-                _signInManager.PasswordSignInAsync(email, password, false, false);
-            }
+        [AllowAnonymous]
+        [HttpGet]
 
-            
-        }*/
+        public IActionResult Login(string returnUrl = "")
+        {
+            var model = new LoginViewModel { ReturnUrl = returnUrl };
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityUser user = await _userManager.FindByNameAsync(model.Username);
+                if (user != null)
+                {
+                    await _signInManager.SignOutAsync();
+                    if ((await _signInManager.PasswordSignInAsync(user, model.Password, false, false)).Succeeded)
+                    {
+                        return Redirect(model?.ReturnUrl ?? "Admin/Index");
+                    }
+                }
+            }
+            ModelState.AddModelError("", "Invalid login attempt");
+            return View(model);
+        }
         
+        [HttpPost]
+        public async Task<RedirectResult> Logout(string returnUrl = "/")
+        {
+            await _signInManager.SignOutAsync();
+            return Redirect(returnUrl);
+        }
+
+        /*[HttpGet]
+        public ViewResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(RegisterViewModel model)
+        {
+        }*/
     }
 }
---
+++ SportShop/Models/IdentitySeedData.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportShop.Models
+{
+    public class IdentitySeedData
+    {
+        private const string adminUser = "Admin";
+        private const string adminPassword = "Admin";
+
+        public static async Task EnsurePopulated(UserManager<IdentityUser> userManager)
+        {
+
+            IdentityUser user = await userManager.FindByIdAsync(adminUser);
+            if (user == null)
+            {
+                user = new IdentityUser("Admin");
+                await userManager.CreateAsync(user, adminPassword);
+            }
+        }
+    }
+}
